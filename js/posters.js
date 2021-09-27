1edb3bb330b450f5f53aadfdeba9888073ba5e54
Emilio Antonucci <emilioantonucci4@gmail.com>
2021-09-27 18:54:02 -0300

    css details

--- js/posters.js
+++ js/posters.js
@@ -5,7 +5,7 @@
 var viewer = OpenSeadragon({
     id: "America",
     prefixUrl: "/openseadragon/images/",
-    tileSources: [{ type: 'image', url:  '/assets/portfolio/posters/posters1.webp' }, { type: 'image', url:  '/assets/portfolio/posters/posters2.webp'}, { type: 'image', url:  '/assets/portfolio/posters/posters3.webp'}, { type: 'image', url:  '/assets/portfolio/posters/posters4.webp'}, { type: 'image', url:  '/assets/portfolio/posters/posters5.webp'}],
+    tileSources: { type: 'image', url:  '/assets/portfolio/posters/posters1.webp' },
     sequenceMode: true,
     degrees: 0,
     showRotationControl: true,
@@ -23,8 +23,7 @@ constrainDuringPan: true,
 var viewer = OpenSeadragon({
     id: "SinTituloPosters",
     prefixUrl: "/openseadragon/images/",
-    tileSources: [{ type: 'image', url:  '/assets/portfolio/posters/posters2.webp' }, { type: 'image', url:  '/assets/portfolio/posters/posters3.webp'}, { type: 'image', url:  '/assets/portfolio/posters/posters4.webp'}, { type: 'image', url:  '/assets/portfolio/posters/posters5.webp'}, { type: 'image', url:  '/assets/portfolio/posters/posters1.webp'}],
-    sequenceMode: true,
+    tileSources: { type: 'image', url:  '/assets/portfolio/posters/posters2.webp' },
     degrees: 0,
     showRotationControl: true,
     gestureSettingsTouch: {
@@ -39,8 +38,7 @@ constrainDuringPan: true,
 var viewer = OpenSeadragon({
     id: "Zapatillas",
     prefixUrl: "/openseadragon/images/",
-    tileSources: [{ type: 'image', url:  '/assets/portfolio/posters/posters3.webp' }, { type: 'image', url:  '/assets/portfolio/posters/posters4.webp'}, { type: 'image', url:  '/assets/portfolio/posters/posters5.webp'}, { type: 'image', url:  '/assets/portfolio/posters/posters1.webp'}, { type: 'image', url:  '/assets/portfolio/posters/posters2.webp'}],
-    sequenceMode: true,
+    tileSources: { type: 'image', url:  '/assets/portfolio/posters/posters3.webp' },
     degrees: 0,
     showRotationControl: true,
     gestureSettingsTouch: {
@@ -55,8 +53,7 @@ constrainDuringPan: true,
 var viewer = OpenSeadragon({
     id: "Talco",
     prefixUrl: "/openseadragon/images/",
-    tileSources: [{ type: 'image', url:  '/assets/portfolio/posters/posters4.webp' }, { type: 'image', url:  '/assets/portfolio/posters/posters5.webp'}, { type: 'image', url:  '/assets/portfolio/posters/posters1.webp'}, { type: 'image', url:  '/assets/portfolio/posters/posters2.webp'}, { type: 'image', url:  '/assets/portfolio/posters/posters3.webp'}],
-    sequenceMode: true,
+    tileSources: { type: 'image', url:  '/assets/portfolio/posters/posters4.webp' },
     degrees: 0,
     showRotationControl: true,
     gestureSettingsTouch: {
@@ -71,8 +68,7 @@ constrainDuringPan: true,
 var viewer = OpenSeadragon({
     id: "SinTituloPosters-2",
     prefixUrl: "/openseadragon/images/",
-    tileSources: [{ type: 'image', url:  '/assets/portfolio/posters/posters5.webp' }, { type: 'image', url:  '/assets/portfolio/posters/posters1.webp'}, { type: 'image', url:  '/assets/portfolio/posters/posters2.webp'}, { type: 'image', url:  '/assets/portfolio/posters/posters3.webp'}, { type: 'image', url:  '/assets/portfolio/posters/posters4.webp'}],
-    sequenceMode: true,
+    tileSources: { type: 'image', url:  '/assets/portfolio/posters/posters5.webp' },
     degrees: 0,
     showRotationControl: true,
     gestureSettingsTouch: {
